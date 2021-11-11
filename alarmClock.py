1c5ceee14567fbe0cf303ad5b73886f704f97126
ArttuRav <75360698+ArttuRav@users.noreply.github.com>
2021-11-12 01:10:22 +0200

    Updated alarmClock.py
    Added option to change alarm sound and ability to hear what they sound like while changing alarm sounds.

--- alarmClock.py
+++ alarmClock.py
@@ -1,19 +1,15 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import StringVar, ttk
 import time
 from threading import Thread
 from datetime import datetime
-import simpleaudio as sa
 import os
+import simpleaudio as sa
 
 class AlarmClock(tk.Tk):
 
     def __init__(self):
         super().__init__()
-
-        # Path for sound file
-        myPath = os.path.abspath('soundEffects\\alarmClockBeep.wav')
-        self.alarmSound = sa.WaveObject.from_wave_file(myPath)
 
         self.aMode = False
 
@@ -23,12 +19,19 @@ class AlarmClock(tk.Tk):
         self.geometry('250x150')
         self['bg'] = 'black'
 
-        # Styling label
-        self.style = ttk.Style(self)
-        self.style.configure(
+        # Styling time label
+        self.styleTimeLabel = ttk.Style(self)
+        self.styleTimeLabel.configure(
             'TLabel',
             background = 'black',
             foreground = 'lightblue')
+
+        # Styling [Set alarm] button
+        self.styleAlarmButton = ttk.Style(self)
+        self.styleAlarmButton.configure(
+            'TButton',
+            font=('Helvetica', 10),
+            foreground = 'black')
 
         # Creating label
         self.label = ttk.Label(
@@ -57,11 +60,22 @@ class AlarmClock(tk.Tk):
         # Creating a button to start timer with
         self.button1 = ttk.Button(
             self,
-            text='SET ALARM', 
+            text='Set alarm', 
             command=lambda:Thread(target=self.alarmMode).start())
 
         # Placing button for starting timer
         self.button1.place(relx=0.09, rely=0.72)
+
+        self.soundMenuOptions = ['Sound1', 'Sound2', 'Sound3']
+
+        # Creating a drop down menu
+        self.selectedAlarmSound = StringVar(self)
+        self.alarmSoundMenu = ttk.OptionMenu(
+            self, 
+            self.selectedAlarmSound,
+            self.soundMenuOptions[0],
+            *self.soundMenuOptions,
+            command=lambda x=None:self.getAlarmSound(self.selectedAlarmSound, self.soundMenuOptions)).place(relx=0.69, rely=0.72)
 
         # Calling an update every second
         self.label.after(1000, self.update)
@@ -69,6 +83,7 @@ class AlarmClock(tk.Tk):
         # Creating a thread for updating label and getting time
         self.newThread1 = Thread(target=self.update).start()
         self.newThread2 = Thread(target=self.getTime).start()
+        self.newThread3 = lambda:Thread(target=self.getAlarmSound).start()
 
     def getTime(self):
         return time.strftime('%H:%M:%S')
@@ -79,25 +94,54 @@ class AlarmClock(tk.Tk):
         self.label.after(1000, self.update)
 
     def alarmMode(self):
-        print('Entering alarm mode...')
-        print('Alarm set at:', self.getTime())
-        
-        self.aMode = True
+        try:
+            self.aMode = True
 
-        hours = int(self.entryH.get())
-        minutes = int(self.entryM.get())
-        seconds = int(self.entryS.get())
+            hours = int(self.entryH.get())
+            minutes = int(self.entryM.get())
+            seconds = int(self.entryS.get())
 
-        print(f'Alarm set for: {hours:02d}:{minutes:02d}:{seconds:02d}')
+            print('Entering alarm mode...')
+            print('Alarm set at:', self.getTime())
 
-        while self.aMode == True:
-            now = datetime.now().time()
-            if now.hour == hours and now.minute == minutes and now.second == seconds:
-                self.aMode = False
-                print('Alarm noises')
-                self.playSound = self.alarmSound.play()
-                self.playSound.wait_done()
-                
+            print(f'Alarm set for: {hours:02d}:{minutes:02d}:{seconds:02d}')
+
+            while self.aMode == True:
+                now = datetime.now().time()
+                if now.hour == hours and now.minute == minutes and now.second == seconds:
+                    print('Alarm noises')
+                    self.getAlarmSound(self.selectedAlarmSound, self.soundMenuOptions)
+                    self.aMode = False
+        except ValueError:
+            print('Unable to set alarm: No time entered.')
+
+    def getAlarmSound(self, selectedAlarmSound, soundMenuOptions):
+        # Paths for sound files
+        alarmSound1Path = os.path.abspath('soundEffects\\alarmClockBeep.wav')
+        alarmSound2Path = os.path.abspath('soundEffects\\alarmClockBeep2.wav')
+        alarmSound3Path = os.path.abspath('soundEffects\\alarmClockBeep3.wav')
+
+        alarmSound1 = sa.WaveObject.from_wave_file(alarmSound1Path)
+        alarmSound2 = sa.WaveObject.from_wave_file(alarmSound2Path)
+        alarmSound3 = sa.WaveObject.from_wave_file(alarmSound3Path)
+
+        if selectedAlarmSound.get() == soundMenuOptions[0] and self.aMode == True:
+            alarmSound1.play() # Playing alarm sound 1
+        if selectedAlarmSound.get() == soundMenuOptions[1] and self.aMode == True:
+            alarmSound2.play()
+        if selectedAlarmSound.get() == soundMenuOptions[2] and self.aMode == True:
+            alarmSound3.play()
+        if self.aMode == False:
+            if selectedAlarmSound.get() == soundMenuOptions[0]:
+                print('Playing alarmSound1')
+                alarmSound1.play()
+            if selectedAlarmSound.get() == soundMenuOptions[1]:
+                print('Playing alarmSound2')
+                alarmSound2.play()
+            if selectedAlarmSound.get() == soundMenuOptions[2]:
+                print('Playing alarmSound3')
+                alarmSound3.play()
+
 
 if __name__ == "__main__":
     clock = AlarmClock()
